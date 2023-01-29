56ac5ef945c8fa27d5ea0095cfb7b567b46c8289
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-29 11:07:15 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,11 +1,3 @@
-#  !/usr/bin/env python3
-#  -*- coding: utf-8 -*-
-#  Name     : broadcast-bot [ Telegram ]
-#  Repo     : https://github.com/m4mallu/broadcast-bot
-#  Author   : Renjith Mangal [ https://t.me/space4renjith ]
-#  Licence  : GPL-3
-
-
 import os
 import logging
 
@@ -21,7 +13,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    API_ID = int(os.environ.get("API_ID", ""))
 
     # Get from my.telegram.org
     API_HASH = os.environ.get("API_HASH", "")
