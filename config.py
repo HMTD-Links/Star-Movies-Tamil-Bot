02863ab083e262d075e2e27572dff47087b742ef
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-29 11:04:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
     # Get from my.telegram.org
     APP_ID = int(os.environ.get("APP_ID", ""))
